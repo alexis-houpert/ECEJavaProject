ce622e3cd7b27f05abc472af7757f241b97de48d
alexis-houpert <houpert.alexis@gmail.com>
2021-07-08 12:55:30 +0200

    process récupération des donnée WIP

---
+++ src/Application/Constantes.java
@@ -0,0 +1,13 @@
+package Application;
+
+public class Constantes {
+
+    // SHOP ITEM CONSTANT
+    public static final String VUE_ITEM_NAME = "#name";
+    public static final String VUE_ITEM_HORSE_POWER = "#horsePower";
+    public static final String VUE_ITEM_NB_SEATS = "#numberSeat";
+    public static final String VUE_ITEM_PRICE = "#price";
+
+
+
+}
